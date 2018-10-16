446d7b579ddb0de03986c1496fdccc7734ff635d
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-16 13:31:14 -0700

    imports NavStyles to Nav

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a73476f9afd4ee4f357";
+/******/ 	var hotCurrentHash = "0f034616a01b78b7cac0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
