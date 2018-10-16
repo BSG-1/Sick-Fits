f17d051e29f00acf97b5348d5e2b98f1eab20d24
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-16 12:01:03 -0700

    creates styled component for background, color, maxwidth etc

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1e4a3b163a307f480b8e";
+/******/ 	var hotCurrentHash = "bd7ee58f6f562445d3df";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
