f2a999ad427ce8c759e4a09a30779067d43d75a0
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-16 12:50:51 -0700

    styles Header.js with styled components

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ba04b4f9c24b6ea73c25";
+/******/ 	var hotCurrentHash = "85d38738f7e27ca2ccdb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
