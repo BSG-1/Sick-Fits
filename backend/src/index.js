4f0ef561f264aefd2aee8a11f81a92bea7a47114
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-23 17:37:19 +0100

    starts index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,10 @@
 // let's go - starts up the node server!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('createServer');
+const db = require('./db');
+
+const server = createServer();
+
+//TODO Use express middleware to handle cookies (JWT)
+
+//TODO Use express middleware to populate current user
