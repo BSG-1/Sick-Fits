bfee5623bc0f8d69bcf946faabeb978c3c346a54
Ghataorhe <b.ghataorhe@gmail.com>
2018-10-23 17:48:54 +0100

    adds scripts to package.json backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,3 +8,13 @@ const server = createServer();
 //TODO Use express middleware to handle cookies (JWT)
 
 //TODO Use express middleware to populate current user
+
+sersver.start({
+    //only be visited from approved urls
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    },
+}, deets => {
+    console.log(`Sever is now running on port http:/localhost:${deets.port}`);
+});
