6c4af4095d648b73daec73ef816c2962a42f20fc
Ghataorhe <b.ghataorhe@gmail.com>
2018-11-11 11:49:48 -0800

    imports createitem to sell page

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3b46c70d37568218261a";
+/******/ 	var hotCurrentHash = "1499631cbbf23ff46c43";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
