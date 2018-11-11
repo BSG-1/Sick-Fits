2d0913be923df0d93e9f6b0ce1958c09343c7f11
Ghataorhe <b.ghataorhe@gmail.com>
2018-11-11 11:48:24 -0800

    basic outlay of sell

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "11169d5863ada99b78aa";
+/******/ 	var hotCurrentHash = "3b46c70d37568218261a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 //allows us to push data and actually make a change
 import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
 
 class CreateItem extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <Form>
+                <h2>Sell an Item.</h2>
+            </Form>
         );
     }
 }
